4064cef309d10bd05a8acb8c71b131643841ded7
Gauthier Dubourdieu <gdubourd@gmail.com>
2022-05-16 18:28:06 +0200

    Exemple de serveur HTTP basique en Python

---
+++ SimpleHTTPRequestHandler.py
@@ -0,0 +1,24 @@
+import http
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from io import BytesIO
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello world !')
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        self.send_response(200)
+        self.end_headers()
+        response = BytesIO()
+        response.write(b'Ceci est une requete POST. ')
+        response.write(b'Donnees recues: ')
+        response.write(body)
+        self.wfile.write(response.getvalue())
+
+httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+httpd.serve_forever()
